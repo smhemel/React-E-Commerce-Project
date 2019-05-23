f2cb39bfa018af4daf0c5c834a7343f1c04f4ceb
S M HEMEL <sm_hemel@yahoo.com>
2019-05-23 21:00:50 +0600

    Product update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { styled } from "styled-components";
+import { Link } from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
 export default class Product extends Component {
   render() {
+    const {id, title, img, price, inCart } = this.props.Product
     return (
-      <div>
-        
-      </div>
-    )
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div className="img-container p-5" onClick={ () => {
+            
+          }} >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top"/>
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
   }
 }
+
+
+const ProductWrapper = styled.div`
+
+
+`
